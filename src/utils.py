e7d5ae4c278813cce0f376443ce10ad2ec37dfee
Aditya <aadi21092005@gmail.com>
2026-02-09 16:06:40 +0530

    model trainer done

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 import os
 import sys
-import numpy as np
-import pandas as pd
-import dill
+import pickle
 
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 
 
+# ================= SAVE OBJECT =================
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,7 +14,27 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+# ================= EVALUATE MODELS =================
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for name, model in models.items():
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
